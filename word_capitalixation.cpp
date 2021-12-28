597821586762cda2369120d7d2af802abe4e17d6
swasthik anchan <60971999+swasthianchankolya@users.noreply.github.com>
2021-12-28 14:15:48 +0530

    Add files via upload

---
+++ word_capitalixation.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+/*
+https://codeforces.com/problemset/problem/281/A
+*/
+int main()
+{
+   string str;
+   cin>>str;
+   str[0]=towupper(str[0]);
+   cout<<str;
+    return 0;
+}
