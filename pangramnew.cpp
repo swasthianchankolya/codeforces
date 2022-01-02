1be180f7b790b2d9befabf3fa746a8b4777c3887
swasthik anchan <60971999+swasthianchankolya@users.noreply.github.com>
2022-01-02 13:29:25 +0530

    Add files via upload

---
+++ pangramnew.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ 
+int main()
+{
+     int number;
+     cin>>number;
+     string element;
+     int count=0;
+ 
+    cin>>element;
+
+    //stl function to convert whole string to lower case
+    transform(element.begin(),element.end(),element.begin(), ::tolower);
+
+    string newelement=element;
+ 
+    for(int i='a';i<='z';i++)
+    {
+        for(int j=0;j<number;j++)
+        {
+            if(i==newelement[j])
+            {
+            count=count+1;
+            break;
+            }
+           
+        }
+    }
+    if(count==26)
+    {
+        cout<<"YES";
+        
+       
+    }
+    else
+    {
+        cout<<"NO";    
+    }
+ 
+    
+    return 0;
+}
